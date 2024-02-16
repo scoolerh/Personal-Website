76893a1e4f1d9245f91a86746221e380c639725b
Hannah Scooler <97634258+scoolerh@users.noreply.github.com>
2024-02-15 18:44:17 -0600

    adding 2024 book spreadsheet

--- src/components/ReadingPage.vue
+++ src/components/ReadingPage.vue
@@ -3,12 +3,16 @@
     <p>View my Goodreads page <a href="https://www.goodreads.com/user/show/159382572-hannah-scooler" target="_blank">here</a>.</p>
     <br /><br />
     <p>View my reading spreadsheet below, where I keep track of the number of pages I read per day and the books I have finished this year:</p>
+    <iframe src="https://docs.google.com/spreadsheets/d/1RKoTTu7cqxKesELZOfISAc0uQJp7TXwz3Y-XMOw9c2I/edit?usp=sharing"
+      width="100%"
+      height="600px">
+    <p>View my spreadsheet from last year:</p>
     <iframe src="https://docs.google.com/spreadsheets/d/12eXpwKyFt2kgMKDa1wiSLOigfN_fBNqogKxI253REBk/preview"
       width="100%"
       height="600px">
     </iframe>
     <br /> <br />
-    <p>View my spreadsheet from last year:</p>
+    <p>View my spreadsheet from 2022:</p>
     <iframe
       src="https://docs.google.com/spreadsheets/d/1ks8l8k5_EXndMzKEY3rIOlYmADwzQwnDmfFLrgvCPFQ/preview"
       width="100%"
